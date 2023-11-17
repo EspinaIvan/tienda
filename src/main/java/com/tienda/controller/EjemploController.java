52e3ec46e0c76da43ac817a8e082fdd5a48f6c70
EspinaIvan <148896876+EspinaIvan@users.noreply.github.com>
2023-11-17 22:01:05 +0100

    Initial commit

---
+++ src/main/java/com/tienda/controller/EjemploController.java
@@ -0,0 +1,21 @@
+package com.tienda.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class EjemploController {
+
+	@GetMapping(value = "/")
+	public String mostrarSaludo(Model model) {
+		// Agregar datos al modelo
+		model.addAttribute("mensaje", "Hola desde GIT");
+
+		// Devolver el nombre de la vista (sin extensión)
+		return "HolaMundo";
+	}
+	
+}
---
+++ src/main/webapp/WEB-INF/views/HolaMundo.jsp
@@ -0,0 +1,9 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Saludo</title>
+</head>
+<body>
+    <h2>${mensaje}</h2>
+</body>
+</html>
